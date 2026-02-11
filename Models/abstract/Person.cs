7a337ecec5c4b22cf6e444c468cd72473e30d124
bispo-eugenio <bispoeugenio270@gmail.com>
2026-02-11 11:04:47 -0300

    feat: adiciona classe abstrata Person

---
+++ Models/abstract/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace schoolApp.Models.@abstract;
+
+public abstract class Person
+{
+    public abstract string FirstName { get; set; }
+    public abstract string LastName { get; set; }
+    public abstract DateTime Birthday { get; set; }
+    public abstract string Cpf { get; set; }
+    public abstract DateTime CreateData { get; set; }
+    public abstract DateTime UpdateData { get; set; }
+
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+    public abstract void SetCreateData();
+    public abstract void SetUpdateData();
+}
