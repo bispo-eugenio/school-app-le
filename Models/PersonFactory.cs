ecc109bb56a23435999e76b2cdd86ff1d174c6e3
bispo-eugenio <bispoeugenio270@gmail.com>
2026-02-21 11:27:00 -0300

    feat: adição da classe PersonFactory para simplificar a criação de entidade

---
+++ Models/PersonFactory.cs
@@ -0,0 +1,57 @@
+﻿using IPersonFactory = schoolApp.Types.IPersonFactory;
+using Stats = schoolApp.Types.Enums.Stats;
+using Group = schoolApp.Types.Enums.Group;
+
+namespace schoolApp.Models;
+
+public class PersonFactory : IPersonFactory
+{
+
+    public PersonFactory() { }
+
+    public Student CreateStudent(string firstname,
+    string lastname, DateTime birthday,
+    string cpf, Stats stats,
+    List<double>? grade = null, Group? group = null)
+    {
+        if (grade != null && group != null)
+        {
+            return new Student(firstname, lastname,
+            birthday, cpf, stats, grade, group.Value);
+        }
+        else if (grade != null)
+        {
+            return new Student(firstname, lastname,
+            birthday, cpf, stats, grade);
+        }
+        else if (group != null)
+        {
+            return new Student(firstname, lastname,
+            birthday, cpf, stats, group.Value);
+        }
+        return new Student(firstname, lastname, birthday, cpf, stats);
+    }
+
+    public Teacher CreateTeacher(string firstname,
+    string lastname, DateTime birthday,
+    string cpf, Stats stats, double? salary = null,
+    List<Student>? classroom = null)
+    {
+        if (salary != null && classroom != null)
+        {
+            return new Teacher(firstname, lastname,
+            birthday, cpf, stats, salary.Value, classroom);
+        }
+        else if (salary != null)
+        {
+            return new Teacher(firstname, lastname, birthday, cpf, stats, salary.Value);
+        }
+        else if (classroom != null)
+        {
+            return new Teacher(firstname, lastname,
+            birthday, cpf, stats, classroom);
+        }
+        return new Teacher(firstname, lastname, birthday, cpf, stats);
+    }
+
+}
---
+++ Types/IPersonFactory.cs
@@ -0,0 +1,19 @@
+﻿using Student = schoolApp.Models.Student;
+using Teacher = schoolApp.Models.Teacher;
+using Stats = schoolApp.Types.Enums.Stats;
+using Group = schoolApp.Types.Enums.Group;
+
+namespace schoolApp.Types;
+
+public interface IPersonFactory
+{
+    Student CreateStudent(string firstname,
+    string lastname, DateTime birthday,
+    string cpf, Stats stats,
+    List<double>? grade = null, Group? group = null);
+    Teacher CreateTeacher(string firstname,
+    string lastname, DateTime birthday,
+    string cpf, Stats stats, double? salary = null,
+    List<Student>? classroom = null);
+
+}
