99f5456067b6cc77c45f6e044cdc7c79c2187707
bispo-eugenio <bispoeugenio270@gmail.com>
2026-03-06 23:10:23 -0300

    feat: adiciona interface IFormatter + classe CPFFormatter

---
+++ Types/IFormatter.cs
@@ -0,0 +1,6 @@
+﻿namespace schoolApp.Types;
+
+public interface IFormatter<T>
+{
+    T Format(T value);
+}
---
+++ Utils/Formatters/CPFFormatter.cs
@@ -0,0 +1,12 @@
+﻿using schoolApp.Types;
+using CPFCNPJ;
+namespace schoolApp.Utils.@Formatters;
+
+public class CPFFormatter : IFormatter<string>
+{
+    private static readonly Main _main = new();
+    public string Format(string value)
+    {
+        return _main.FormatCPFCNPJ(value, CPFCNPJ.Enum.TypeString.CPF);
+    }
+}
